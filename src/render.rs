7f0a8318c1142130b4eea3317213a82a8de24370
Jax-Hax <jaxhacks543@gmail.com>
2023-10-28 11:33:37 -0400

    i added in the length thing but it sitll doesnt work idk why

--- src/render.rs
+++ src/render.rs
@@ -44,14 +44,13 @@ pub fn render(state: &mut State) -> Result<(), wgpu::SurfaceError> {
         render_pass.set_bind_group(1, &state.camera.bind_group, &[]);
         for (_, game_object) in &instance_updater.prefab_slab {
             render_pass.set_vertex_buffer(1, game_object.buffer.slice(..));
-            println!("test");
             render_pass.set_vertex_buffer(0, game_object.mesh.vertex_buffer.slice(..));
             render_pass.set_index_buffer(
                 game_object.mesh.index_buffer.slice(..),
                 wgpu::IndexFormat::Uint32,
             );
             render_pass.set_bind_group(0, &game_object.mesh.material.bind_group, &[]);
-            render_pass.draw_indexed(0..game_object.mesh.num_elements, 0, 0..1);
+            render_pass.draw_indexed(0..game_object.mesh.num_elements, 0, 0..game_object.length);
         }
     }
 
