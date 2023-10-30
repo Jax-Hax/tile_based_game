573a5ba3863a36a2338a84c9ec4cf60aea3bd43b
Jax-Hax <jaxhacks543@gmail.com>
2023-10-30 17:53:49 -0400

    working on player movement

--- src/tile_game/player.rs
+++ src/tile_game/player.rs
@@ -1,4 +1,8 @@
-use bevy_ecs::system::Resource;
+use bevy_ecs::system::{Resource, Query, ResMut, Res};
+use tile_based_game::resources::WindowEvents;
+use winit::event::VirtualKeyCode;
+
+use super::terrain::World;
 
 #[derive(Resource)]
 pub struct Player {
@@ -9,4 +13,18 @@ impl Player {
         let (x,y) = self.position;
         (x.round() as u32,y.round() as u32)
     }
+}
+fn player_movement(window_events: Res<WindowEvents>, mut world: ResMut<World>, mut asset_server: ResMut<Player>) {
+    if window_events.is_key_pressed(VirtualKeyCode::D, None) {
+        
+    }
+    /*if collider.check_collision(instance, &window_events) && window_events.left_clicked() {
+        for chunk_row in &world.chunks {
+            for chunk in chunk_row {
+                if chunk.rendered {println!("{}", chunk.prefab_idx);asset_server.remove_prefab(chunk.prefab_idx);}
+            }
+        }
+        let mut world = gen(1000, 500, rand::thread_rng().gen_range(0..100000), world.sprite_map_idx);
+        world.save_to_image("output.png");
+    }*/
 }
